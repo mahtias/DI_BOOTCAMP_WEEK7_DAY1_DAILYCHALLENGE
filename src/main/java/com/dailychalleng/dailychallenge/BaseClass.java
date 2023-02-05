b810be090abb285be7c4b790618ab782c27c95a6
Konate Mamadou <120653410+MamadouKernel@users.noreply.github.com>
2023-02-05 13:20:36 +0000

    Add files via upload

---
+++ src/main/java/com/dailychalleng/dailychallenge/BaseClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dailychalleng.dailychallenge;
+
+/**
+ *
+ * @author konate-mk
+ */
+public class BaseClass {
+    
+    public void display() {
+        System.out.println("Méthode de la classe de base");
+    }
+    
+}
---
+++ src/main/java/com/dailychalleng/dailychallenge/Dailychallenge.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dailychalleng.dailychallenge;
+
+/**
+ *
+ * @author kernel-mk
+ */
+public class Dailychallenge {
+
+    public static void main(String[] args) {
+        
+        DerivedClass derivedObject = new DerivedClass();
+        derivedObject.display();
+    
+    }
+}
